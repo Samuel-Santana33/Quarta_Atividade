2567438bd8dd39a8d5d072725f4a44f443408f50
Samuel-Santana33 <santana.samuel.2033@gmail.com>
2022-11-23 16:43:55 -0400

    Atualização

---
+++ Quarta_Atividade/src/herancas_em_java/MaquinaLavarRoupa.java
@@ -0,0 +1,28 @@
+package herancas_em_java;
+public class MaquinaLavarRoupa extends Aparelho{
+    private double capacidade = 5;
+
+    public MaquinaLavarRoupa() {
+    }
+
+    public MaquinaLavarRoupa(double peso, double preco) {
+        super(peso, preco);
+    }
+
+    public MaquinaLavarRoupa(double peso, String cor, char consumoEnergia, double preco, double capacidade) {
+        super(peso, cor, consumoEnergia, preco);
+        this.capacidade = capacidade;
+    }
+
+    @Override
+    public void precoFinal() {
+        super.precoFinal();
+        if (capacidade > 30) {
+            setPreco(getPreco()+500);
+        }
+    }
+
+    public double getCapacidade() {
+        return capacidade;
+    }
+}
---
+++ Quarta_Atividade/src/herancas_em_java/Principal.java
@@ -0,0 +1,27 @@
+package herancas_em_java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        List<Aparelho> lista = Arrays.asList(
+                new Televisao(50, 1200), new MaquinaLavarRoupa(20, 2000), new Aparelho(),
+                new Televisao(40, 3000), new MaquinaLavarRoupa(), new MaquinaLavarRoupa(),
+                new Aparelho(), new Televisao(50, 5000), new Televisao(20, 2000), new Aparelho()
+        );
+
+        for (Aparelho a : lista) {
+            a.precoFinal();
+        }
+
+        for (Aparelho a : lista) {
+            System.out.println("Valor final: " + a.getPreco() + ".\nAparelho: " + a);
+            System.out.println("\n\n");
+        }
+    }
+    
+    
+}
